95956f03553c7cc3f9d8b84804cbfe6cf2a6ad23
rostikKalabishka <rostik31052002@gmail.com>
2023-09-19 19:59:52 +0300

    added modelModels

--- lib/models/models_model.dart
+++ lib/models/models_model.dart
@@ -3,5 +3,19 @@ class ModelsModel {
   final int created;
   final String root;
 
-  ModelsModel({required this.id, required this.created, required this.root});
+  ModelsModel({
+    required this.id,
+    required this.created,
+    required this.root,
+  });
+
+  factory ModelsModel.fromJson(Map<String, dynamic> json) => ModelsModel(
+        created: json['created'] as int,
+        id: json['id'] as String,
+        root: json['root'] as String,
+      );
+
+  static List<ModelsModel> modelsFromSnapshot(List modelSnapshot) {
+    return modelSnapshot.map((data) => ModelsModel.fromJson(data)).toList();
+  }
 }
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,14 +1,17 @@
 import 'dart:convert';
+import 'dart:developer';
 import 'dart:io';
 
 import 'package:chat_gpt/constants/api_consts.dart';
 import 'package:http/http.dart' as http;
 
+import '../models/models_model.dart';
+
 class ApiService {
-  static Future<void> getModels() async {
+  static Future<List<ModelsModel>> getModels() async {
     try {
       final response = await http.get(
-        Uri.parse('https://api.openai.com/v1/models'),
+        Uri.parse('$BASE_URL/models'),
         headers: {'Authorization': 'Bearer $API_KEY'},
       );
       Map jsonResponse = jsonDecode(response.body);
@@ -17,9 +20,16 @@ class ApiService {
         print(jsonResponse['error']['message']);
         throw HttpException(jsonResponse['error']['message']);
       }
-      print(jsonResponse);
+
+      // print(jsonResponse);
+      List temp = [];
+      for (var value in jsonResponse['data']) {
+        temp.add(value);
+      }
+      return ModelsModel.modelsFromSnapshot(temp);
     } catch (e) {
-      print('error $e');
+      log('error $e');
+      rethrow;
     }
   }
 }
